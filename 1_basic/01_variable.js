1087ad0e5976d6765b60b15e20afd5f797f8eafb
Vickram666 <vickramraghuwansy@gmail.com>
2024-07-30 03:09:58 +0000

    Project

---
+++ 1_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountID = 666111
+let accountemail ="rtherat@gmail.com" 
+var accountpassword = "123456"
+accountcity = "bangalore"
+
+/*please dont use var as once you changes thing it will be change in all the page */ 
+
+accountemail = "muzaffar.com"
+accountpassword = "654321"
+accountcity = "muzaf"
+
+console.table([accountID, accountemail, accountpassword, accountcity])
